9d60a51a3d146005f9c958c5b0354c9c36e75eb4
AndrewMatheny <github email address>
2020-02-07 13:41:58 -0800

    Fixed to extend component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 
-class App extends Component {
+class App extends React.Component {
 
   // getTrips = () => {
   //   fetch(`localhost`)
