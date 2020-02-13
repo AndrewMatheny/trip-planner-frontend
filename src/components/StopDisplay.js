4f67ad0733be18608262f0fe1dac966c45f37afe
NicoleLC16 <asalwaysnicole@outlook.com>
2020-02-12 17:37:48 -0800

    styling stops

--- src/components/StopDisplay.js
+++ src/components/StopDisplay.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
+import { Card } from 'semantic-ui-react'
 
 class StopDisplay extends Component {
   render() {
     return(
-      <div>
-        <p>{this.props.stop.name}<br></br>
+      <div className="ui column" style={{padding: '1.5rem'}}>
+        <Card raised style={{height: '150px'}}>
+          <Card.Content>
+          <Card.Header>{this.props.stop.name}</Card.Header>
+        <Card.Description>
         Location: {this.props.stop.location}<br></br>
         Date: {this.props.stop.location}<br></br>
         Notes: {this.props.stop.notes}
-        </p>
+        </Card.Description>
+        </Card.Content>
+          </Card>
       </div>
     )
   }
--- src/containers/StopsContainer.js
+++ src/containers/StopsContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import StopDisplay from '../components/StopDisplay'
 import CreateStop from './CreateStop'
+import { Container } from 'semantic-ui-react'
 
 class StopsContainer extends Component {
   stopsCollection = () => {
@@ -13,15 +14,17 @@ class StopsContainer extends Component {
 
   render() {
     return(
-      <div>
-        <div>
-          <h2>Itinerary</h2>
-          {this.stopsCollection()}
+      <Container style={{margin: '40px'}} >
+            <h2>Itinerary</h2>
+        <div className="ui three column grid">
+          <div className="row"> 
+              {this.stopsCollection()}
+          </div>
         </div>
         <div>
           <CreateStop user={this.props.user} trip={this.props.trip} updateStops={this.props.updateStops}/>
         </div>
-      </div>
+      </Container>
     )
   }
 }
